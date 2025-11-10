8957316de2e4d9abfbd459ee4f6dc7481257c263
Erick Santos Brito <101837070+ericksntz@users.noreply.github.com>
2025-11-10 17:47:26 -0300

    [Feat]: Inicio primeira função de menu principal

---
+++ Aulas/Projeto - Cinemack/projeto_cinemack.py
@@ -0,0 +1,3 @@
+def menu_principal():
+    opc = int(input("1. Comprar ingressos para Filme 1 - Sessão 1\n2. Comprar ingressos para Filme 1 - Sessão 2\n3. Comprar ingressos para Filme 2 - Sessão 1\n4. Comprar ingressos para Filme 2 - Sessão 2\n5. Comprar ingressos para Filme 3 - Sessão 1\n6. Comprar ingressos para Filme 3 - Sessão 2\n7. Avaliar um filme\n8. Encerrar o dia e exibir o relatório"))
+    
